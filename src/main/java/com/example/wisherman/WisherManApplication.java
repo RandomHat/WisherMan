6a85237c435d4a9e4575be4269e2e46d81833ec0
Simon Roswall <simonroswall@gmail.com>
2021-11-01 11:15:22 +0100

    created project structure with spring

---
+++ src/main/java/com/example/wisherman/WisherManApplication.java
@@ -0,0 +1,13 @@
+package com.example.wisherman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisherManApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WisherManApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wisherman/WisherManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wisherman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisherManApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
