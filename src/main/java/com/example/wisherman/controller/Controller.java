1214faa416a7a3c6d1b71fc161b6e1e29e3f2fb3
Mark Hansen <Mark024e@stud.kea.dk>
2021-11-02 12:58:52 +0100

    DB Connection

--- src/main/java/com/example/wisherman/controller/Controller.java
+++ src/main/java/com/example/wisherman/controller/Controller.java
@@ -1,4 +1,10 @@
 package com.example.wisherman.controller;
 
+import com.example.wisherman.utlility.DBConnection;
+
+import java.sql.Connection;
+
 public class Controller {
+
+    Connection conn = DBConnection.getConnection();
 }
